edf2a74ae0f2804ce8e618473f182cae3e5655e8
najiro8v <harolantonio77@outlook.com>
2019-01-27 13:38:49 -0600

    comunicacion para la app de techo

---
+++ app/src/main/java/com/example/estacion01/techo/Presentacion.java
@@ -0,0 +1,13 @@
+package com.example.estacion01.techo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Presentacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentacion);
+    }
+}
